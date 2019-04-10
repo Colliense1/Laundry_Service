5f81740682bbb85cae463f79337e61c33ec585e9
shipon0142 <shipon0142@gmail.com>
2019-04-10 13:22:46 +0600

    test

--- app/src/main/java/com/example/colliensepodder/laundry/Activity/LaundryShopList.java
+++ app/src/main/java/com/example/colliensepodder/laundry/Activity/LaundryShopList.java
@@ -17,6 +17,7 @@ public class LaundryShopList extends AppCompatActivity {
     public RecyclerView recyclerview_shoplist;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
